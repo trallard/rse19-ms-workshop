13d0c991802a5957bde07d327799ea94eea4e859
Matthew Johnson (HU) <matjoh@microsoft.com>
2019-09-16 00:52:51 +0100

    Addnig command code

--- bokehpreview/extension.ts
+++ bokehpreview/extension.ts
@@ -1,6 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as cp from 'child_process';
+
+let serverProcess: cp.ChildProcess;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,15 +20,60 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.bokehPreview', () => {
 		// The code you place here will be executed every time your command is executed
 
-		const panel = vscode.window.createWebviewPanel(
-			'bokehPreview', // Identifies the type of the webview. Used internally
-			'Bokeh Preview', // Title of the panel displayed to the user
-			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-			{} // Webview options. More on these later.
-		);
+		let output = getOutputChannel();
+		let activeEditor = vscode.window.activeTextEditor;
+		output.appendLine("Examining file...");
+		if(!activeEditor){
+			output.appendLine("No active editor");
+			return;
+		}
 
-		// And set its HTML content
-		panel.webview.html = getWebviewContent();
+		let doc = activeEditor.document;
+		let start = doc.fileName.length - "main.py".length;
+		if(doc.fileName.substring(start) === "main.py"){
+			output.appendLine("main.py detected");
+			let text = doc.getText();
+			let import_bokeh = text.indexOf("from bokeh.io import curdoc") >= 0;
+			let add_root = text.indexOf("curdoc().add_root(") >= 0;
+			let dir = doc.fileName.substring(0, start);
+			if(import_bokeh && add_root){
+				output.appendLine("Starting server...");
+				let pythonConf = vscode.workspace.getConfiguration("python");
+				if(!pythonConf){
+					output.appendLine("Python not configured");
+					return;
+				}
+
+				let command = pythonConf.get<string>("pythonPath");
+				if(!command){
+					output.appendLine("No environment found, using global python");
+					command = "python";
+				}
+
+				let args = [
+					"-m",
+					"bokeh",
+					"serve",
+					dir,
+					"--dev"
+				];
+				serverProcess = cp.spawn(command, args);
+
+				serverProcess.stdout.on('data', (data) => {
+					output.append(`${data}`);
+				});
+				
+				serverProcess.stderr.on('data', (data) => {
+					output.append(`${data}`);
+				});
+				
+				serverProcess.on('close', (code) => {
+					output.append(`child process exited with code ${code}`);
+				});
+			}
+		}else{
+			output.appendLine("Not a bokeh file");
+		}
 	});
 
 	context.subscriptions.push(disposable);
@@ -33,16 +82,21 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-function getWebviewContent() {
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <body>
-	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
-  </body>
-  </html>`;
-  }
+let _channel: vscode.OutputChannel;
+function getOutputChannel(): vscode.OutputChannel {
+	if (!_channel) {
+		_channel = vscode.window.createOutputChannel('Bokeh');
+	}
+	return _channel;
+}
+
+function exec(command: string, options: cp.ExecOptions): Promise<{ stdout: string; stderr: string }> {
+	return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
+		cp.exec(command, options, (error, stdout, stderr) => {
+			if (error) {
+				reject({ error, stdout, stderr });
+			}
+			resolve({ stdout, stderr });
+		});
+	});
+}
