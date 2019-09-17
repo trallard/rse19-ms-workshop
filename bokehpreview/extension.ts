016068ecc9820f2a450c416050fa68477f4f0f01
Matthew Johnson (HU) <matjoh@microsoft.com>
2019-09-17 16:13:52 +0100

    Minor tweaks

--- bokehpreview/extension.ts
+++ bokehpreview/extension.ts
@@ -126,7 +126,7 @@ function getWebviewContent() {
   <head>
 	  <meta charset="UTF-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
+	  <title>Bokeh Preview</title>
   </head>
   <body>
   	<iframe src="http://localhost:5006/" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
